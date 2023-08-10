2b64e502ea7522f19abe61489457dc2d1e3ed021
AtxGamer <jambhulkaranurag12@gmail.com>
2023-08-10 22:12:59 +0530

    release

---
+++ app/src/main/java/com/example/quizappanuragjambhulkar/QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quizappanuragjambhulkar;
+
+
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "How many planet in Our Solar System?",
+            "Which one is not the programming language?",
+            "Which planet is the biggest in our Solar System",
+            "which company provided the IOS UI"
+    };
+
+    public static String choices[][] = {
+            {"3","8","6","5"},
+            {"Java","Kotlin","HTML","Python"},
+            {"Jupiter","Mars","Earth","Venus"},
+            {"Google","Samsung","Nokia","Apple"}
+    };
+
+    public static String correctAnswers[] = {
+            "8",
+            "HTML",
+            "Jupiter",
+            "Apple"
+    };
+
+}
